6b60f4f64cafbfaed50fdc483cd631a323e649e9
bvenna <bvenna@gmail.com>
2022-12-30 10:34:03 -0800

    extent report

--- src/test/java/GoogleSearch.java
+++ src/test/java/GoogleSearch.java
@@ -1,13 +1,29 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.List;
 public class GoogleSearch {
+    ExtentHtmlReporter htmlReporter;
+    ExtentReports extent;
+
+    @BeforeTest
+    public void setUp(){
+        htmlReporter = new ExtentHtmlReporter("extent.html");
+        // create ExtentReports and attach reporter(s)
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
     @Test
     public void searchTest() {
+        ExtentTest test = extent.createTest("MyFirstTest", "Google Search");
         System.setProperty("webdriver.chrome.driver", "C://Selenium//chromedriver_win32//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
@@ -30,10 +46,14 @@ public class GoogleSearch {
             if (itemlist.contains("java compiler")){
                 list.get(i).click();
                 System.out.println("Item selected from the list is: " + itemlist);
+                test.pass("Test Passed");
                 break;
             }
-
         }
         driver.quit();
+    }
+    @AfterTest
+    public void tearDown(){
+        extent.flush();
     }
 }
